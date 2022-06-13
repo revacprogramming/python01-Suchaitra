b4fc4d0bd21dc644b66fea3e42baa139a9576708
suchaitra Manchal <96183999+Suchaitra@users.noreply.github.com>
2022-06-13 15:14:34 +0000

    file

--- ActivitySet01/problem10.py
+++ ActivitySet01/problem10.py
@@ -1,3 +1,8 @@
-# Dictionaries
-
-filename = "dataset/mbox-short.txt"
+while true:
+  inp = input('enter the numbers:')
+  if inp== 'done' : break
+    value = float(inp)
+    total=total+value
+    count=count+1
+    average=total/count
+   print('average:',average)
--- ActivitySet01/temp.py
+++ ActivitySet01/temp.py
@@ -17,3 +17,5 @@ print(x)
 
 fruit="banana"
 print(len(fruit))
+
+
